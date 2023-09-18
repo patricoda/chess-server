ae07c88fcc4c2413686f088f28838697082209c2
Patrick Craig <patrickb@olmgroup.com>
2023-09-18 17:23:49 +0100

    Implement awaiting game listener, ensure users join lobby on awaiting game

--- src/index.js
+++ src/index.js
@@ -17,13 +17,32 @@ server.listen(3001, () => {
 //TODO: currently will lose game on refresh.. really need to keep socket id and game id stored client-side
 //that way we can get the game state (for both player and spectators), and if socket id is a player, then just carry on as norm \o/
 //https://socket.io/get-started/private-messaging-part-2/
-io.on("connection", async (socket) => {
-  console.log("a user connected");
-  socket.join("lobby");
-  console.log(socket.id, "waiting in lobby");
+io.on("connection", (socket) => {
+  console.log(`new socket connection - ${socket.id}`);
 
   socket.on("disconnect", () => {
     console.log(`${socket.id} disconnected`);
+  });
+
+  socket.on("AWAITING_GAME", async () => {
+    socket.join("lobby");
+    console.log(socket.id, "waiting in lobby");
+
+    const waitingPlayers = await io.in("lobby").fetchSockets();
+
+    if (waitingPlayers.length > 1) {
+      const playersToAction = waitingPlayers.slice(0, 2);
+      const roomId = randomUUID();
+
+      for (player of playersToAction) {
+        player.join(roomId);
+        console.log(player.id, `- joining game ${roomId}`);
+        player.leave("lobby");
+        console.log(player.id, `- leaving lobby`);
+        //TODO: on game join, pass default state, store room and state somewhere
+        player.emit("GAME_JOINED", { gameId: roomId });
+      }
+    }
   });
 
   socket.on("POST_MESSAGE", (message) => {
@@ -39,20 +58,4 @@ io.on("connection", async (socket) => {
     //on post move, update room's game state, and emit the state to all users in room.
     io.to(socket.rooms).emit("GAME_UPDATED");
   });
-
-  const waitingPlayers = await io.in("lobby").fetchSockets();
-
-  if (waitingPlayers.length > 1) {
-    const playersToAction = waitingPlayers.slice(0, 2);
-    const roomId = randomUUID();
-
-    for (player of playersToAction) {
-      player.join(roomId);
-      console.log(player.id, `- joining game ${roomId}`);
-      player.leave("lobby");
-      console.log(player.id, `- leaving lobby`);
-      //TODO: on game join, pass default state, store room and state somewhere
-      player.emit("GAME_JOINED", { gameId: roomId });
-    }
-  }
 });
